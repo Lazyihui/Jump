722f397f8d0dabad786c57f4431fae621ce6570d
Lazyihui <chengbaozi6@qq.com>
2025-04-10 16:42:27 +0800

    ESTABLISH

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("PlayerController started");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
