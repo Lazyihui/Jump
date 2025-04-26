4f14fce1ca0da27f8591d990f0d8944517686c97
Lazyihui <chengbaozi6@qq.com>
2025-04-26 15:24:21 +0800

    <feature>add: 可以跳跃了

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	// public GameManager gameManager;
+
+	// void Awake()
+	// {
+	// 	// Awake 比 Start 先执行
+	// 	gameManager = this;
+	// }
+
+	public static GameManager S;
+
+	void Awake()
+	{
+		S = this;
+	}
+
+	public bool PlayerIsFacingXAxis;
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,66 @@
 ﻿using System;
+using System.Security.Policy;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    // Use this for initialization
-    void Start () {
-        Debug.Log("PlayerController started");
+    Rigidbody rb;
+
+    float timer;
+
+    public float maxChargeTime;
+    bool doJump;
+    bool isGround = true;
+
+    public float jumpPower;
+
+    Vector3 originalScale;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = 1; // 确保质量合理（默认1）
+        rb.drag = 0;  // 空气阻力设为0
+        originalScale = transform.localScale;
     }
 
-    // Update is called once per frame
-    void Update () {
-        
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.Mouse0) && timer < maxChargeTime)
+        {
+            timer += Time.deltaTime;
+            // 根据 timer 的蓄力进度（timer / maxChargeTime）计算压缩比例
+            // 根据 timer 动态计算压缩比例（范围：0.5f ~ 1）
+            float compressRatio = Mathf.Lerp(0.5f, 1f, 1 - (timer / maxChargeTime));
+            transform.localScale = new Vector3(1, compressRatio * originalScale.y, 1); // 压缩Player
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            doJump = true;
+            transform.localScale = new Vector3(1, 1, 1);//恢复Player
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (doJump)
+        {
+            Jump();
+            doJump = false;
+            isGround = false;
+            timer = 0;
+        }
+    }
+    void Jump()
+    {
+        Vector3 dir = GameManager.S.PlayerIsFacingXAxis ? Vector3.right : Vector3.forward;
+        dir.y = 1;
+        dir = dir.normalized; // 归一化确保力度一致
+        rb.velocity = dir * jumpPower * timer;
+        Debug.Log("Jump Power: " + (dir * jumpPower * timer));
     }
 }
